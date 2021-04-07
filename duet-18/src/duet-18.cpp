dddc5040db8e85c1fdd5d9e6654613b84d3ea2a4
MaverickTheDude <38852811+MaverickTheDude@users.noreply.github.com>
2021-04-07 13:46:01 +0200

    duet (18 b)
    tagi jak w 18a

--- duet-18/src/duet-18.cpp
+++ duet-18/src/duet-18.cpp
@@ -3,7 +3,8 @@
 #include <regex>
 #include <vector>
 #include <map>
-#include <string>
+#include <queue>
+#include <unistd.h>
 using std::cout;
 using std::endl;
 
@@ -14,11 +15,11 @@ uint64_t constexpr hash(const char * m)    {return (*m) ? mix(*m,hash(m+1)) : 0;
 enum commands {snd, set, add, mul, mod, rcv, jgz};
 enum states {_none_, _value_, _register_};
 
-static std::map<char, long long int> registers;
-static int frequency = 0;
-
-void add_to_map (char reg);
-
+static int executions = 0;
+const int NCOMMANDS = 41; // hardcoded
+static std::regex wzorzec("([a-z]+) ([a-z1-9]) ?(-?[a-z0-9]+)?");
+using reg_map = std::map<char, long long int>;
+using reg_queue = std::queue<long long int>;
 
 class argument {
 friend std::ostream& operator<<(std::ostream& os, const argument& obj);
@@ -28,15 +29,15 @@ public:
 	argument() : value(0), reg('0'), state(_none_) {
 		cout << "unused, right?" << endl; }  // note: tego konstruktora dotyczy obserwacja ponizej
 
-	long long int get_value() const {
+	long long int get_value(reg_map *const reg_ptr) const {
 		if (state == _none_) throw "this argument is empty!";
-		return state == _value_ ? value : registers[reg];
+		return state == _value_ ? value : (*reg_ptr)[reg];
 	}
 
-	void set(long long int new_value) {
+	void set(long long int new_value, reg_map *const reg_ptr) {
 		if (state == _none_)  throw "this argument is empty!";
 		if (state == _value_) throw "there is no register to set!";
-		registers[reg] = new_value;
+		(*reg_ptr)[reg] = new_value;
 	}
 
 private:
@@ -53,11 +54,12 @@ public:
 	// note: copy - constructor powinien przyjmowac argument const& obj
 	instruction(const instruction& ins) : function(ins.function), arg1(ins.arg1), arg2(ins.arg2) {}
 
-	void print() const  {cout << "function id: " << function << arg1 << arg2 << endl; }
+	int print(int id, int line, reg_map *const reg_ptr) const;
+
 	void print(std::ofstream& file) const
 		{file << "function id: " << function << arg1 << arg2 << endl; }
 
-	int execute(bool* continue_);
+	int execute(reg_queue& ours, reg_queue *const theirs, reg_map *const ptr, const int&);
 
 private:
 	int function;
@@ -65,40 +67,67 @@ private:
 	argument arg2;
 };
 
+class program {
+public:
+	program(std::ifstream& file, int _id_ = 0);
+
+	int execute_line(int line) {
+		return commands[line].execute(queue, co_q_ptr, &registers, id);
+	}
+
+	void print_line(int line) {
+		long long int x = commands[line].print(id, line, &registers);
+		cout << "reg. value: " << x << endl;
+	}
+
+	void print_commands(std::ofstream& outfile) const {
+		for (auto &x : commands)
+			x.print(outfile);
+	}
+
+	void connect(program& program) {
+		co_q_ptr = &program.queue;
+		program.co_q_ptr = &this->queue;
+	}
+
+private:
+	void add_to_map(char);
+
+	int id;
+	std::map<char, long long int> registers;
+	std::vector<instruction> commands;
+	std::queue<long long int> queue;
+	std::queue<long long int>* co_q_ptr;
+};
+
 int main() {
-	std::string line;
 	std::ifstream myfile("src/input.txt");
 	std::ofstream outfile("src/output.txt");
-	std::smatch wynik;
-	std::regex wzorzec("([a-z]+) ([a-z1-9]) ?(-?[a-z0-9]+)?");
 
-	std::vector<instruction> commands;
+	program prog0(myfile);
+	myfile.clear();				 // re-read input file
+	myfile.seekg(0, myfile.beg);
+	program prog1(myfile, 1);
+	prog0.connect(prog1);
 
-	while ( getline(myfile, line) ) {
-		std::regex_search(line, wynik, wzorzec);
-		commands.emplace_back(wynik);
+	int current_line0 = 0;
+	int current_line1 = 0;
+	while (true) {
+
+		const int next0 = prog0.execute_line(current_line0);
+		const int next1 = prog1.execute_line(current_line1);
+		current_line0 += next0;
+		current_line1 += next1;
+
+		if (next0 == 0 && next1 == 0)	/* deadlock */		break;
+		if (current_line0 < 0 || current_line0 > NCOMMANDS) break;
+		if (current_line1 < 0 || current_line1 > NCOMMANDS) break;
 	}
 
-	const int coms_size = commands.size();
-	bool work = true;
-	int current_line = 0;
-	while (work) {
-		current_line += commands[current_line].execute(&work);
+	prog1.print_commands(outfile);
 
-		// debug statements:
-/*		for (auto& x : registers)
-			cout << x.first << " " << x.second << '\t';
-		cout << endl << endl;*/
-
-		if (current_line < 0 || current_line > coms_size)
-			break;
-	}
-
-
-	for (auto &x : commands)
-		x.print(outfile);
-
-	cout << "last frequency: " << frequency << endl;
+	cout << "send exectuions by program #1: " << executions <<  endl;
+	myfile.close();
 	return 0;
 }
 
@@ -124,39 +153,44 @@ argument::argument(std::string in_str) {
 		return;
 	}
 	/* argument is a register */
-	add_to_map(reg);
 	value = 0;
 	state = _register_;
 }
 
-int instruction::execute(bool* continue_) {
+int instruction::execute(reg_queue& ours, reg_queue *const theirs, reg_map *const ptr, const int& id) {
 	long long int new_val;
 	switch (function) {
 		case snd :
-			frequency = arg1.get_value();
-			break;
-		case set :
-			arg1.set(arg2.get_value());
-			break;
-		case add :
-			new_val = arg1.get_value() + arg2.get_value();
-			arg1.set(new_val);
-			break;
-		case mul :
-			new_val = arg1.get_value() * arg2.get_value();
-			arg1.set(new_val);
-			break;
-		case mod :
-			new_val = arg1.get_value() % arg2.get_value();
-			arg1.set(new_val);
+			/* wysylamy do kolejki partnera */
+			theirs->push( arg1.get_value(ptr) );
+			if (id == 1)	++executions;
 			break;
 		case rcv :
-			if (arg1.get_value() > 0)
-				*continue_ = false;
+			/* pobieramy ze swojej kolejki */
+			if (ours.size() > 0) {
+				arg1.set(ours.front(), ptr);
+				ours.pop();
+				return 1;
+			}
+			else return 0;
+		case set :
+			arg1.set(arg2.get_value(ptr), ptr);
+			break;
+		case add :
+			new_val = arg1.get_value(ptr) + arg2.get_value(ptr);
+			arg1.set(new_val, ptr);
+			break;
+		case mul :
+			new_val = arg1.get_value(ptr) * arg2.get_value(ptr);
+			arg1.set(new_val, ptr);
+			break;
+		case mod :
+			new_val = arg1.get_value(ptr) % arg2.get_value(ptr);
+			arg1.set(new_val, ptr);
 			break;
 		case jgz :
-			if (arg1.get_value() > 0)
-				return arg2.get_value();
+			if (arg1.get_value(ptr) > 0)
+				return arg2.get_value(ptr);
 			break;
 		default : throw "execute: unresolved case";
 	}
@@ -187,11 +221,38 @@ instruction::instruction(std::smatch arglist)
 //	arg2 = argument(arglist[3]);
 }
 
-void add_to_map (char reg) {
-	std::map<char, long long int>::const_iterator it;
+int instruction::print(int id, int line, reg_map *const reg_ptr) const {
+	std::string fun;
+	switch (function) {
+		case snd : fun = "snd";	break;
+		case set : fun = "set";	break;
+		case add : fun = "add";	break;
+		case mul : fun = "mul";	break;
+		case mod : fun = "mod";	break;
+		case rcv : fun = "rcv";	break;
+		case jgz : fun = "jgz";	break;
+		default  : throw "error";
+	}
+	cout << "id/line: " << id << "/" << line <<
+			" function: " << fun << " " << arg1 << arg2;
+	return arg1.get_value(reg_ptr);
+}
+
+void program::add_to_map (char reg) {
+	reg_map::const_iterator it;
 	it = registers.find(reg);
 	if (it == registers.end())
 		registers[reg] = 0;
+}
+
+program::program(std::ifstream& file, int _id_) : id(_id_), co_q_ptr(nullptr) {
+	std::string line;
+	std::smatch wynik;
+	while ( getline(file, line) ) {
+		std::regex_search(line, wynik, wzorzec);
+		commands.emplace_back(wynik);
+	}
+	registers['p'] = _id_;
 }
 
 std::ostream& operator<<(std::ostream& os, const argument& obj)  {
@@ -202,3 +263,5 @@ std::ostream& operator<<(std::ostream& os, const argument& obj)  {
 	}
 	return os;
 }
+
+
