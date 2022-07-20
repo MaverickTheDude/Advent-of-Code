e7dfc0cd26dde9024295865ec7de4644c9fef859
Maverick <pwmaciag@outlook.com>
2022-07-20 12:31:01 +0200

    sporfica-virus-22 (a, b)
    tags:
     - std::map<pair, bool> (find, at vs operator[])
     - trick: arithmetics on directions via modulus (like in clocks, but mod4 instead of mod12)

--- sporfica-virus-22/main.cpp
+++ sporfica-virus-22/main.cpp
@@ -6,32 +6,47 @@ using std::cout;
 using std::endl;
 using std::string;
 using point = std::pair<int,int>;
-using gridType = std::map<point, bool>;
+using gridType = std::map<point, int>;
 void printMap(const gridType& grid);
 inline point move(const point& pt, int direction);
-enum direction {north, east, south, west}; // clock-based modulus arithmetics -> mod 4
-const int Nsteps = 10000;
+point simulateA(const point& pt, gridType& grid);
+enum direction {north = 0, east, south, west}; // clock-based modulus arithmetics -> mod 4
+enum states {clean, weakened, infected, flagged};
+const int Nsteps = 10000000;
 int INFECTIONS = 0;
 
-point simulate(const point& pt, gridType& grid) {
+point simulateB(const point& pt, gridType& grid) {
     static int direction = north;
-    bool isInfected = grid.at(pt);
-    if (isInfected)
-        direction = (direction + 1) % 4; // turn right
-    else {
-        direction = (direction + 3) % 4; // turn left
-        INFECTIONS++;
+    int status = grid.at(pt);
+    int newStatus;
+    switch (status) {
+        case infected:   
+            direction = (direction + 1) % 4; // turn right
+            newStatus = flagged;
+            break;
+        case clean:
+            direction = (direction + 3) % 4; // turn left
+            newStatus = weakened;
+            break;
+        case flagged:
+            direction = (direction + 2) % 4; // turn around
+            newStatus = clean;
+            break;
+        case weakened:
+            newStatus = infected;
+            INFECTIONS++;
+            break;
+        default:
+            throw std::runtime_error("unexpected status'\n");
     }
-    grid[pt] = !isInfected;
+    grid[pt] = newStatus;
 
     point newPoint = move(pt, direction);
     if (grid.find(newPoint) == grid.end())    // if pt not in grid ...
-        grid[newPoint] = false;
+        grid[newPoint] = clean;
 
     return newPoint;
 }
-
-
 
 int main() {
     string line;
@@ -44,8 +59,8 @@ int main() {
     while( getline(myfile, line) ) {
         x = -12;
         for (char symbol : line) {
-            const bool isInfected = (symbol == '#' ? true : false);
-            grid[std::make_pair(x,y)] = isInfected;
+            const int nodeStatus = (symbol == '#' ? infected : clean);
+            grid[std::make_pair(x,y)] = nodeStatus;
             ++x;
         }
         --y;
@@ -53,12 +68,30 @@ int main() {
 
     point position = std::make_pair(0,0);
     for (int i = 0; i < Nsteps; ++i)
-        position = simulate(position, grid);
+        position = simulateB(position, grid); // case A: simulateA(...) / Nsteps=1e5
 
     printMap(grid);
     std::cout << "\n\nposition = (" << position.first << ", " << position.second << ")\n";
     std::cout << endl << "total infections = " << INFECTIONS << std::endl;
     return 0;
+}
+
+point simulateA(const point& pt, gridType& grid) {
+    static int direction = north;
+    int status = grid.at(pt);
+    if (status == infected)
+        direction = (direction + 1) % 4; // turn right
+    else {
+        direction = (direction + 3) % 4; // turn left
+        INFECTIONS++;
+    }
+    grid[pt] = (status == infected ? clean : infected);
+
+    point newPoint = move(pt, direction);
+    if (grid.find(newPoint) == grid.end())    // if pt not in grid ...
+        grid[newPoint] = clean;
+
+    return newPoint;
 }
 
 inline point move(const point& pt, int direction) {
@@ -78,8 +111,15 @@ void printMap(const gridType& grid) {
         for (int x = -12; x < 13; ++x) {
             point pt = std::make_pair(x,y);
             if (grid.find(pt) != grid.end()) { // if pt in grid ...
-                bool isInfected = grid.at(pt);
-                char symbol = (isInfected ? '#' : '.');
+                int status = grid.at(pt);
+                char symbol;
+                switch (status) {
+                    case clean:    symbol = '.'; break;
+                    case weakened: symbol = 'w'; break;
+                    case infected: symbol = '#'; break;
+                    case flagged:  symbol = 'f'; break;
+                    default: throw std::runtime_error("unexpected status'\n");
+                }
                 std::cout << symbol;
             }
             else
